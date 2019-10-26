4a37d978f3f62353638284d6a82ad138e949b689
Gary Soedarsono <garysoed@gmail.com>
2019-10-26 15:10:25 -0700

    help cli

--- src/cli/help.ts
+++ src/cli/help.ts
@@ -1,14 +1,40 @@
-import * as chalk from 'chalk';
+import * as commandLineArgs from 'command-line-args';
 
 import { CommandType } from './command-type';
+import { printSummary } from './print-summary';
+
+const COMMAND_OPTION = 'command';
+const OPTIONS = [
+  {
+    name: COMMAND_OPTION,
+    defaultOption: true,
+  },
+];
 
 export const CLI = {
   title: 'Hive: Help',
   body: () => ({
     header: 'COMMANDS',
     content: [
+      {name: CommandType.HELP, summary: CLI.summary},
     ],
   }),
   summary: 'Display help on commands',
   synopsis: `$ hive ${CommandType.HELP} <command>`,
 };
+
+export function help(argv: string[]): string {
+  const options = commandLineArgs(OPTIONS, {argv, stopAtFirstUnknown: true});
+  switch (options[COMMAND_OPTION]) {
+    // case CommandType.ANALYZE:
+    //   return printSummary(ANALYZE_CLI);
+    case CommandType.HELP:
+      return printSummary(CLI);
+    // case CommandType.INIT:
+    //   return printSummary(INIT_CLI);
+    // case CommandType.RENDER:
+    //   return printSummary(RENDER_CLI);
+    default:
+      return printSummary(CLI);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,42 @@
 import chalk from 'chalk';
+import * as commandLineArgs from 'command-line-args';
 
 import { formatMessage, MessageType } from '@gs-tools/cli';
 import { Observable, of as observableOf } from '@rxjs';
 
-import { CLI as HELP_CLI } from './cli/help';
+import { CommandType } from './cli/command-type';
+import { CLI as HELP_CLI, help } from './cli/help';
 import { printSummary } from './cli/print-summary';
 
-
+const COMMAND_OPTION = 'command';
+const OPTIONS = [
+  {
+    name: COMMAND_OPTION,
+    defaultOption: true,
+  },
+];
 const CLI = {
   body: HELP_CLI.body,
   summary: `{bold hive} - Manages a chain of rendering processes to render your documents.`,
   synopsis: `$ {bold hive} {underline command} [command options]`,
 };
 
+const options = commandLineArgs(OPTIONS, {stopAtFirstUnknown: true});
+
 /**
  * Returns observable that emits messages to print
  */
 function run(): Observable<string> {
-  return observableOf(printSummary(CLI));
+  switch (options[COMMAND_OPTION]) {
+    // case CommandType.ANALYZE:
+    //   return analyze(options._unknown || []);
+    case CommandType.HELP:
+      return observableOf(help(options._unknown || []));
+    // case CommandType.INIT:
+    //   return init(options._unknown || []);
+    default:
+      return observableOf(printSummary(CLI));
+  }
 }
 
 // tslint:disable: no-console
