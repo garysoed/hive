25a5c1460e673271738088f5d49536994d2e0366
Gary Soedarsono <garysoed@gmail.com>
2020-06-14 23:33:21 -0700

    Updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-const WebpackBuilder = require('dev/webpack/builder');
+const webpackBuilder = require('dev/webpack/builder');
 const glob = require('glob');
 
-const base = (new WebpackBuilder(__dirname))
-    .addEntry('test', glob.sync('./src/**/*.test.ts'))
-    .addEntry('main', glob.sync('./src/main.ts'))
-    .setOutput('[name].js', '/out')
-    .addTypeScript()
-    .addHtml()
-    .setAsNode()
-    .buildForDevelopment('Hive');
+module.exports = webpackBuilder(__dirname)
+    .forDevelopment('[default]', builder => builder
+        .addEntry('test', glob.sync('./src/**/*.test.ts'))
+        .addEntry('main', glob.sync('./src/main.ts'))
+        .setOutput('[name].js', '/out')
+        .addTypeScript()
+        .addHtml()
+        .setAsNode()
+    )
+    .build();
 
 // Needed because command-line-usage assumes that array-back is commonjs, but array-back ends up
 // with ejs in module mode.
-base.resolve.mainFields = ['main', 'module'];
-module.exports = base;
+// base.resolve.mainFields = ['main', 'module'];
